99779449441e94d0c1efe6576896662576ef6a3a
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-08-28 15:54:55 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      /**
+       * First up is the WordPress source plugin that connects Gatsby
+       * to your WordPress site.
+       *
+       * visit the plugin docs to learn more
+       * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
+       *
+       */
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // the only required plugin option for WordPress is the GraphQL url.
+        url: process.env.WPGRAPHQL_URL || `http://gatsbywp.local/graphql`,
+      },
+    },
+  ],
+}
